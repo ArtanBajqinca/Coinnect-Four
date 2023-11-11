4d39cdc5c9ccaa26bcaa856931cd928b7184e105
Lavdim Ismaili <lavdim_milan@hotmail.com>
2023-11-11 15:38:33 +0100

    Update MainActivity.kt

--- app/src/main/java/artan/lavdim_connect_4_group_4/MainActivity.kt
+++ app/src/main/java/artan/lavdim_connect_4_group_4/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("LavdimNatiadaasdon")
                     Text(text = "poggers")
-                    Text(text = "Merge test")
                 }
             }
         }
