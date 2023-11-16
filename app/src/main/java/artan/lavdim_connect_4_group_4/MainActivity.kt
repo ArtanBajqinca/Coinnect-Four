a69cc5abb2a594a634bd4b82e86f1bee94aeeb83
Lavdim Ismaili <lavdim_milan@hotmail.com>
2023-11-16 11:12:34 +0100

    ui theme removed

--- app/src/main/java/artan/lavdim_connect_4_group_4/MainActivity.kt
+++ app/src/main/java/artan/lavdim_connect_4_group_4/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import artan.lavdim_connect_4_group_4.Screens.Navigation
-import artan.lavdim_connect_4_group_4.ui.theme.Artanlavdim_Connect_4_Group_4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/artan/lavdim_connect_4_group_4/ui/theme/Color.kt
+++
@@ -1,5 +0,0 @@
-package artan.lavdim_connect_4_group_4.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-
