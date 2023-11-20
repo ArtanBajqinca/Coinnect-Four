dcdfc90d2e19d3cf6056c1911f3841c113e40e60
Arctan <artan.bajqinca@gmail.com>
2023-11-20 15:13:34 +0100

    Create ViewModel folder structure

--- app/src/main/java/artan/lavdim_connect_4_group_4/screens/FontStyles.kt
+++ app/src/main/java/artan/lavdim_connect_4_group_4/screens/FontStyles.kt
@@ -1,19 +1,19 @@
-//package artan.lavdim_connect_4_group_4.screens
-//
-//
-//import androidx.compose.material3.Typography
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.text.TextStyle
-//import androidx.compose.ui.text.font.Font
-//import androidx.compose.ui.text.font.FontFamily
-//import androidx.compose.ui.text.font.FontWeight
-//import androidx.compose.ui.unit.sp
-//import artan.lavdim_connect_4_group_4.R
-//
-//val AvenirRoundedFontFamily = FontFamily(
-//    Font(R.font.avenir_rounded_normal, FontWeight.Normal),
-//    Font(R.font.avenir_rounded_bold, FontWeight.Bold)
-//)
+package artan.lavdim_connect_4_group_4.screens
+
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import artan.lavdim_connect_4_group_4.R
+
+val AvenirRoundedFontFamily = FontFamily(
+    Font(R.font.avenir_rounded_normal, FontWeight.Normal),
+    Font(R.font.avenir_rounded_bold, FontWeight.Bold)
+)
 //
 //val AvenirTypograhy = Typography(
 //    regular = TextStyle(
