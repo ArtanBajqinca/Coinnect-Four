b64450571d3b3f4eff8ff7f97fd8733635987381
Lavdim Ismaili <lavdim_milan@hotmail.com>
2023-12-02 09:22:40 +0100

    Sort viewmodels

---
+++ app/src/main/java/artan/lavdim_connect_4_group_4/viewModels/SharedViewModel.kt
@@ -0,0 +1,32 @@
+package artan.lavdim_connect_4_group_4.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.garrit.android.multiplayer.Game
+import io.garrit.android.multiplayer.Player
+import io.garrit.android.multiplayer.SupabaseService
+import kotlinx.coroutines.launch
+
+class SharedViewModel : ViewModel() {
+
+    fun joinLobby(player: Player){
+        viewModelScope.launch {
+            SupabaseService.joinLobby(player)
+        }
+    }
+    fun invite(player: Player){
+        viewModelScope.launch {
+            SupabaseService.invite(player)
+        }
+    }
+    fun declineInvite(game: Game){
+        viewModelScope.launch {
+            SupabaseService.declineInvite(game)
+        }
+    }
+    fun acceptInvite(game: Game){
+        viewModelScope.launch {
+            SupabaseService.acceptInvite(game)
+        }
+    }
+}
